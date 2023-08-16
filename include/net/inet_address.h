e1bed81e477929abd494892018c937f12bddba75
liuzy <jenningsliu@163.com>
2023-08-17 01:14:14 +0800

    start of the net lib

--- include/net/inet_address.h
+++ include/net/inet_address.h
@@ -1,17 +1,70 @@
 //
-// Created by A on 2023/8/16.
+// Created by lzy on 2023/8/16.
 //
 
 #ifndef LIZLIB_INET_ADDRESS_H
 #define LIZLIB_INET_ADDRESS_H
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include "common/basic.h"
+#include "common/logger.h"
+#include "common/slice.h"
 namespace lizlib {
+
 class InetAddress {
  public:
+  InetAddress() = default;
+  InetAddress(const std::string& host, uint16_t port, bool ipv6 = false);
+  InetAddress(const InetAddress& addr) = default;
+
+  InetAddress(InetAddress&& addr) noexcept
+      : host_(std::move(addr.host_)), ipv6_(addr.ipv6_) {
+    std::swap(impl_, addr.impl_);
+  }
+
+  ~InetAddress() = default;
+
+  InetAddress& operator=(InetAddress&& addr);
+  InetAddress& operator=(const InetAddress& addr);
+
+  [[nodiscard]] bool Ipv6() const noexcept { return ipv6_; }
+  [[nodiscard]] int Family() const noexcept {
+    return ipv6_ ? AF_INET6 : AF_INET;
+  }
+
+  [[nodiscard]] const std::string& Host() const noexcept { return host_; }
+  /**
+   * the returned value will meet the platform's endian
+   * @return
+   */
+  [[nodiscard]] int Port() const noexcept;
+
+  [[nodiscard]] std::string String() const noexcept {
+    return fmt::format("InetAddress[{}:{}]", host_, Port());
+  }
+
+  [[nodiscard]] Slice Data() const { return Slice{&impl_, sizeof(sockaddr)}; }
 
  private:
+  /**
+ * what need to be noted that net processing in linux is set to use big
+ * endian format, so the stuffed data need to be transformed first, namely
+ * htobexx
+ */
+  union InetImpl {
+    struct sockaddr_in6 in6;
+    struct sockaddr_in in4;
+  };
+
   friend class Socket;
+  std::string host_;
+  bool ipv6_;
+  InetImpl impl_{};
 };
+
 }  // namespace lizlib
+
+FORMATTER_REGISTRY(lizlib::InetAddress);
 
 #endif  //LIZLIB_INET_ADDRESS_H
--- include/net/socket.h
+++ include/net/socket.h
@@ -8,23 +8,42 @@
 #include <netinet/in.h>
 #include "common/basic.h"
 #include "common/file.h"
+#include "inet_address.h"
 
 namespace lizlib {
 
 class Socket : public File {
  public:
   Socket() : Socket(-1) {}
-  Socket(Socket&& other) noexcept : File(std::forward<File>(other)) {}
+  Socket(Socket&& other) noexcept
+      : File(std::forward<File>(other)) {}
   Socket& operator=(Socket&& other) noexcept;
 
   static Socket Create(int domain, bool nonblock, int protocol = IPPROTO_TCP);
 
+  void Bind(const InetAddress& addr);
 
+  void Listen();
+
+  Status Accept(InetAddress* remote, Socket* socket);
+
+  Status Connect(const InetAddress& address) {
+    Slice impl = address.Data();
+    Socket gen = Socket {
+      ::connect(fd_, reinterpret_cast<sockaddr*>(impl.Data()), impl.Length());
+    };
+  };
+
+  [[nodiscard]] std::string String() const override {
+    return fmt::format("[Socket{}]", fd_);
+  }
 
  private:
   explicit Socket(int fd) : File(fd) {}
 };
 
 }  // namespace lizlib
+
+FORMATTER_REGISTRY(lizlib::Socket);
 
 #endif  //LIZLIB_SOCKET_H
